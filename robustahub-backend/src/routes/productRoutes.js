de891423943b712cbd6474cf4a7b4cb51bfc3519
Fayiz-Akbar <fayizakbar26@gmail.com>
2026-04-19 13:41:09 +0700

    penambahan id di product routes

--- robustahub-backend/src/routes/productRoutes.js
+++ robustahub-backend/src/routes/productRoutes.js
@@ -1,12 +1,16 @@
 const express = require('express');
-const { createProduct, getAllProducts } = require('../controllers/productController');
-// 1. Panggil satpam isPetani
+// 1. Panggil fungsi barunya
+const { createProduct, getAllProducts, updateProduct, deleteProduct } = require('../controllers/productController');
 const { verifyToken, isPetani } = require('../middlewares/authMiddleware'); 
 
 const router = express.Router();
 
-// 2. Pasang isPetani di sini (Harus login DULU, baru dicek apakah dia Petani)
 router.post('/', verifyToken, isPetani, createProduct); 
 router.get('/', getAllProducts);
+
+// 2. Tambahkan jalur Update & Delete (Hanya Petani yang boleh akses)
+// Tanda /:id berarti URL-nya butuh ID produk, contoh: /api/products/123-abc
+router.put('/:id', verifyToken, isPetani, updateProduct);
+router.delete('/:id', verifyToken, isPetani, deleteProduct);
 
 module.exports = router;
