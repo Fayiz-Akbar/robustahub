ec5d0255d0827ae326d1f67d72ac993350214e65
Fayiz-Akbar <fayizakbar26@gmail.com>
2026-04-30 06:45:20 +0700

    penambahan cors di kode index.js

--- robustahub-backend/src/index.js
+++ robustahub-backend/src/index.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const path = require('path'); // Tambahkan baris ini!
+const path = require('path');
 
-// ... (kode sisanya biarkan sama)
 // Mengimpor jalur (routes) yang baru dibuat
 const authRoutes = require('./routes/authRoutes');
 const productRoutes = require('./routes/productRoutes');
@@ -15,10 +14,16 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+// KONFIGURASI CORS YANG BARU
+app.use(cors({
+  origin: 'http://localhost:5173', 
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
+
 app.use(express.json());
 
-// Memasang jalur utama untuk API Autentikasi
+// Memasang jalur utama untuk API
 app.use('/api/auth', authRoutes);
 app.use('/api/products', productRoutes);
 app.use('/api/orders', orderRoutes);
